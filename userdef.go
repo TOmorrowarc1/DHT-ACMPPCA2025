eaba83e9cab0d6a49b763f80cf5549b3bebd7ee9
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-06-18 14:58:09 +0800

    feat: start to build my own node.

--- userdef.go
+++ userdef.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"dht/naive"
+	"dht/node"
 )
 
 /*
@@ -12,7 +12,7 @@ import (
 
 func NewNode(port int) dhtNode {
 	// Todo: create a node and then return it.
-	node := new(naive.Node)
+	node := new(node.Node)
 	node.Init(portToAddr(localAddress, port))
 	return node
 }
